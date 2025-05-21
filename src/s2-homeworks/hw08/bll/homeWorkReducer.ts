739f32a2ec15dfb47e2cdad48a3ab15f96548662
Ilja <chylmanprosto@gmail.com>
2025-05-21 10:21:33 +0300

    fix: hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,19 +1,20 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-            const sortType = action.payload
-          return  sortType === "up" ? [...state.sort((a, b) => a.name.localeCompare(b.name))] : [...state.sort((a, b) => b.name.localeCompare(a.name))]// need to fix
-        }
-        case 'check': {
-            return state.filter(people => people.age >= action.payload) // need to fix
-        }
-        default:
-            return state
+  switch (action.type) {
+    case 'sort': { // by name
+      const sortType = action.payload
+      const newState = [...state]
+      return sortType === "up" ? newState.sort((a, b) => a.name.localeCompare(b.name)) : newState.sort((a, b) => b.name.localeCompare(a.name))// need to fix
     }
+    case 'check': {
+      return state.filter(people => people.age >= action.payload) // need to fix
+    }
+    default:
+      return state
+  }
 }
